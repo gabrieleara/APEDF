d0ac6c8379ffef0d9922867c4c845a826870a824
Gabriele Ara <gabriele.ara@santannapisa.it>
2023-01-19 15:49:26 +0000

    Generating new tasksets (longer)

--- tasksets/params.sh
+++ tasksets/params.sh
@@ -3,7 +3,7 @@
 
 export GT_OUT_DIR="./tasksets"
 export GT_NUM_TASKS_LIST=(6 8 12 16)
-export GT_NUM_TASKSETS="3"
+export GT_NUM_TASKSETS="10"
 export GT_UTILS_NUM="22"
 export GT_UMIN_FRAC="0.25"
 export GT_UMAX_FRAC="0.95"
@@ -11,6 +11,7 @@ export GT_UTILS_LIST=(1.0 1.15 1.3 1.45 1.6 1.75 1.9 2.05 2.2 2.35 2.5 2.6 2.7 2
 export GT_SEED="13994"
 export GT_SEEDS_LIST=(1459 1462 1576 6730 5343 3051 348 940 3523 5784)
 export GT_RT_FRACTION=".95"
+export GT_RT_REMOVE="2000"
 export GT_RT_MIN_DURATION="20"
 export GT_RT_MAX_DURATION="30"
 export GT_RT_CALIBRATION="92"
